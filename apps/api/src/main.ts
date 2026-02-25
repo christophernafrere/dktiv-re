fe44d7d7d84cc0c42b0e0eacfe17ad8a9d5715e5
christophernafrere <nafrere.christopher@gmail.com>
2026-02-25 12:57:50 +0100

    enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,11 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+        credentials: true,
+    });
     app.use(cookieParser());
-    await app.listen(process.env.PORT ?? 4000);
+    await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
